4ec23e9082fb09eb8c48f5c9a9041d44cd793429
Jonathon Stillson <jcstillson77@gmail.com>
2023-06-20 18:22:05 -0400

    front end add and delete

--- src/App.js
+++ src/App.js
@@ -14,33 +14,87 @@ class App extends React.Component {
       books: [],
     }
   }
-
+}
   //To do Handler to get all books
-  
-  render(){
-    return (
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<BestBooks />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-          <Footer />
-        </Router>
-    );
+getAllBooks = async () => {
+  try {
+    let url = `${process.env.REACT_APP_SERVER}/books`
+
+    let booksFromDB = await axios.get(url);
+
+    this.setState({
+      books: booksFromDB.data
+    })
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+componentDidMount() {
+  this.getAllBooks();
+}
+
+/** Add Book To DB using 2 handlers */
+
+/** Handler 1 */
+
+handleBookSubmit = (event) => {
+  event.preventDefault();
+  // TODO: BUILD THE Book OBJECT USING THE FORM DATA
+
+  let bookObj = {
+    title: event.target.title.value,
+    description: event.target.description.value,
+    author: +event.target.author.value,
+    status: event.target.status.value
+  }
+
+  this.postBook(bookObj);
+}
+
+/** 2nd Handler */
+postBook = async (bookObj) => {
+  try {
+    // TODO: build the url for axios to use
+    let url = `${process.env.REACT_APP_SERVER}/books`;
+
+    // *** ON A POST - axios will take in 2 args. 1st - url, 2nd - data which is carried 
+    //over on the req.body
+    let createdBookFromDB = await axios.post(url, bookObj);
+
+    // console.log(createdBookFromDB.data);
+
+    // TODO: get the created book and add it to state
+    this.setState({
+      books: [...this.state.books, createdBookFromDB.data]
+    })
+
+
+    // OR this.getBooks()
+
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+/** Deleting a Book From the DB */
+deleteBook = async (id) => {
+  try {
+    // TODO: build the url for axios 
+    let url = `${process.env.REACT_APP_SERVER}/books/${id}`
+
+    await axios.delete(url);
+
+    // TODO: update state after book was deleted
+    let updatedBooks = this.state.books.filter(book => book._id !== id);
+
+    this.setState({
+      books: updatedBooks
+    })
+
+  } catch (error) {
+    console.log(error.message)
   }
 }
 
 export default App;
-
-
-/*
-{
-  this.state.books.length > 0 &&
-  <>
-    {this.state.books.map(book => {
-      return <p key={book._id}>{book.name} is a {book.description}</p>
-    })}
-  </>
-}
-*/
